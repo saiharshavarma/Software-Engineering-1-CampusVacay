8a6a854d65ec54e43189fb448ea1217b1ca42f94
Harsha <77226384+saiharshavarma@users.noreply.github.com>
2024-10-06 21:20:53 -0400

    Update urls.py

--- backend/student/urls.py
+++ backend/student/urls.py
@@ -12,13 +12,7 @@ from .views import UserRegistrationView, LogoutView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('api/register/', StudentRegistration.as_view(), name='student-registration'),
-    path('api/login/', StudentLogin.as_view(), name='student-login'),
-    path('api/logout/', StudentLogout.as_view(), name='student-logout'),
-=======
     path('api/register/', UserRegistrationView.as_view(), name='user-register'),
     path('api/login/', obtain_auth_token, name='api_login'),
     path('api/logout/', LogoutView.as_view(), name='api_logout'),
->>>>>>> api
 ]

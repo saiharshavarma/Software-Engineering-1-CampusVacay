03e19a6d75afc2f39eac5deedef44910c5d6a621
Harsha <77226384+saiharshavarma@users.noreply.github.com>
2024-10-06 21:22:58 -0400

    Update urls.py

--- backend/student/urls.py
+++ backend/student/urls.py
@@ -1,13 +1,4 @@
 from django.urls import path
-from .views import StudentRegistration, StudentLogin, StudentLogout
-
-# urlpatterns = [
-#     path('register/', views.student_registration, name='student_registration'),
-#     path('login/', views.student_login, name='student_login'),
-#     path('logout/', views.student_logout, name='student_logout'),
-# ]
-
-from django.urls import path
 from .views import UserRegistrationView, LogoutView
 from rest_framework.authtoken.views import obtain_auth_token
 
